a493feefd396f9994cc2382deace7021fbc64104
Mae Capozzi <maecapozzi@gmail.com>
2017-05-28 16:25:51 -0400

    Remove unnecessary import

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import beerApp from './reducers'
 import App from './components/App/App'
-import { addBeer } from './actions'
-
 
 let store = createStore(beerApp)
 
